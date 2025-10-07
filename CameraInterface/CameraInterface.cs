9aa6832d553dd2ce2824c8c6fe4c6eeb8274c578
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2025-10-07 10:17:12 +0200

    Adds trigger configuration interface
    Introduces an interface for defining camera trigger configurations,
    allowing for different triggering mechanisms such as software or
    no trigger setups. This provides a flexible way to configure
    how the camera captures images.

--- CameraInterface/CameraInterface.cs
+++ CameraInterface/CameraInterface.cs
@@ -3,6 +3,31 @@ using UnitsNet;
 namespace CameraInterface;
 
 /// <summary>
+/// Represents a configuration interface for camera triggering mechanisms. Classes or records
+/// implementing this interface define the behavior of specific trigger modes that the camera
+/// can use, such as software-based or no trigger configurations.
+/// </summary>
+public interface ITriggerConfig
+{
+    
+}
+
+/// <summary>
+/// Represents a trigger configuration where no external or software trigger
+/// is used. This configuration can be useful for cameras operating in free-run
+/// mode or scenarios where no explicit triggering mechanism is required.
+/// </summary>
+public sealed record NoTrigger()               : ITriggerConfig;
+
+/// <summary>
+/// Represents a trigger configuration that uses software-based triggering mechanisms.
+/// This configuration enables the camera to be triggered programmatically through
+/// software commands, offering precise control over when captures are initiated.
+/// see <see cref="ICamera.FireManualTrigger"/>.
+/// </summary>
+public sealed record SoftwareTrigger()         : ITriggerConfig;
+
+/// <summary>
 /// Defines the interface for interacting with a camera. Provides properties for
 /// configuring camera settings such as resolution, offsets, exposure time, and
 /// obtaining information about configurable ranges. Also includes functionality
@@ -71,6 +96,14 @@ public interface ICamera
     /// such as whether the format is monochrome or RGB with varying bit depths.
     /// </summary>
     public ImageFormat CameraOutputFormat { get; set; }
+
+    /// <summary>
+    /// Configures the trigger mechanism for the camera based on the provided
+    /// trigger configuration. This method allows the camera to be set up with
+    /// a specific triggering mode, such as software or hardware-based triggering.
+    /// </summary>
+    /// <param name="cfg">The trigger configuration that defines the behavior of the camera's triggering mechanism.</param>
+    public void SetupTrigger(ITriggerConfig cfg);
 
     /// <summary>
     /// Triggers a manual image capture.
